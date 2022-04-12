86c4e21cf044ebbdee63cd44ac035576a0ecda20
SugarRey <shugarprogram1998@gmail.com>
2022-04-11 22:22:31 -0500

    creacion del directorio delcurso de backend con python

---
+++ SEMANA1/DIA1/monedas.py
@@ -0,0 +1,24 @@
+#Programa para convertir monedas
+#version 1.0
+#Input - entradas
+
+montoOrigen = input("Ingrese el moto en soles: ")
+
+#proceso
+print("Opcion 1 = soles a dolares")
+print("opcion 2 = dolares a soles")
+opcion = input("Elija una opcion: ")
+if(opcion =="1"):
+   montoDolares = float(montoOrigen) / 3.8
+   montoDolaresFormato = "$ {:,.2f}".format(montoDolares)
+   #output - salidas
+   print("el monto e n dolares es: " + str(montoDolaresFormato))
+else:
+   montoSoles = float(montoOrigen) * 3.8
+   montoSolesFormato = "S/. {:,.2f}".format(montoSoles)
+   #output - salidas
+   print("el monto en soles es: ", montoSolesFormato)
+
+
+
+
